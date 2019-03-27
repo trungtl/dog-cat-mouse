893e8cb6bd6bd716707a6c2dabcafd629b8e671f
dangtrung <phieuyet@gmail.com>
2019-03-27 10:30:59 +0700

    add sayHi method for dof

--- dog.js
+++ dog.js
@@ -1,8 +1,16 @@
+var chalk = require('chalk');
+
 function Dog(name) {
-	this.name = name
+	this.name = name;
 	this.stomach = [];
 }
 
 Dog.prototype.eat = function(cat) {
 	this.stomach.push(cat);
+};
+
+Dog.prototype.sayHi = function() {
+	console.log('hi! i am a dog. My name is' + chalk.red(this.name));
 }
+
+module.exports = Dog;
